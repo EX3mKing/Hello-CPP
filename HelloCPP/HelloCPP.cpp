b34dc4e1ea05e2ec5b7241b63e67cf2ee2f16d35
Boki <79083667+EX3mKing@users.noreply.github.com>
2024-04-21 19:19:40 +0200

    just some goofying arounf

--- HelloCPP/HelloCPP.cpp
+++ HelloCPP/HelloCPP.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
-int main()
+// comment a
+// comment b */ comment c */
+/*
+ * comment d
+*/                                  
+
+void doNothing(int&){}
+
+void doSomething()
 {
     std::cout << "Hello, World!" << std::endl;
+    [maybe_unused] int w = 3;
+    [maybe_unused] int q;
+    int x {};
+    std::cout << "var x: " << x << "\n";
+    std::cin >> x;
+    std::cout << "var x: " << x << '\n';
+    
+    int z;
+    doNothing(z);
+    std::cout << "var z: " << z << '\n';
+}
+
+int main()
+{
+    doSomething();
     return 0;
 }
